caf27ff58ae18efd207d1e1b3232ee32506085e0
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2024-03-05 14:18:49 +0530

    removed code of Karthik's feature

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,3 @@
 class Calculator {
-    public double multiply(double... numbers) {
-        if (numbers.length == 0) {
-            return 0;
-        }
-        double result = 1.0;
-        for (double number : numbers) {
-            result *= number;
-        }
-        return result;
-    }
+
 }
