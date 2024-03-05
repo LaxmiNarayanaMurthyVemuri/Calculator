bf892d88fd7ecf702cfa7c615cb0f492bdd80853
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-03-05 14:25:23 +0530

    division method called in Main method

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     Calculator calculator = new Calculator();
     // Your code for testing the remaining methods goes here
-
+    int result = calculator.division(6, 3);
+    System.out.println(result);
 }
