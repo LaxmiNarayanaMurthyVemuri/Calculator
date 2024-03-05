bafd08a7ccbb8672a4e22379d1f09e95d6ae981c
Deeraj Kumar <dheerajkumarvirat18@msitprogram.net>
2024-03-05 14:36:58 +0530

    testcases included

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     Calculator calculator = new Calculator();
     // Your code for testing the remaining methods goes here
+    int a = calculator.modulus(5,6);
+    System.out.println(a);
+    int b = calculator.modulus(15,6);
+    System.out.println(b);
+    int c = calculator.modulus(5,16);
+    System.out.println(c);
 }
 
