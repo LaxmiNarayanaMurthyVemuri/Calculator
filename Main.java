b5a3fe9fcbdba15a185e7ebb579fd282b950aec0
Harshitha-Renikindi <hrenikinidi@msitprogram.net>
2024-03-05 14:28:54 +0530

    function for power of no

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     Calculator calculator = new Calculator();
     // Your code for testing the remaining methods goes here
+    public int power(int a,int b){
+        
+    }
 }
 
