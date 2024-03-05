a856486dd3e5a4d0ab59f3f2d13f1483b3d7a0f7
mahavir karthik <mahavirkarthik27@msitprogram.net>
2024-03-05 13:59:22 +0530

    multiplication function done

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,12 @@
 class Calculator {
-    
+    public double multiply(double... numbers){
+        if(numbers.length==0){
+            return 0;
+        }
+        double result=1.0;
+        for (double number:numbers){
+            result*=number;
+        }
+        return result;
+    }
 }
