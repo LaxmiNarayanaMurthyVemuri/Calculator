fc802215ea3d020a751b530a9b1312bfb7482c58
Harshitha-Renikindi <hrenikinidi@msitprogram.net>
2024-03-05 14:36:59 +0530

    committed changes

--- Main.java
+++ Main.java
@@ -1,12 +1,6 @@
 public class Main {
     Calculator calculator = new Calculator();
     // Your code for testing the remaining methods goes here
-    public int power(int a,int b){
-        int ans=a;
-        for(int i=0;i<b;i++){
-            ans=ans*a;
-        }
-        return ans;
-    }
+    
 }
 
