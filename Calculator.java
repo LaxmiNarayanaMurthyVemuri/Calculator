3986b009c7325a763ce1e03e75e63ca3044b87ed
mahavir karthik <mahavirkarthik27@msitprogram.net>
2024-03-05 14:00:53 +0530

    test cases done

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,11 @@
 class Calculator {
-    public double multiply(double... numbers){
-        if(numbers.length==0){
+    public double multiply(double... numbers) {
+        if (numbers.length == 0) {
             return 0;
         }
-        double result=1.0;
-        for (double number:numbers){
-            result*=number;
+        double result = 1.0;
+        for (double number : numbers) {
+            result *= number;
         }
         return result;
     }
