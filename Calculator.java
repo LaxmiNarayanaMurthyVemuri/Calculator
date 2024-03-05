af777717bed47f8206dbb9d204ca11fc99fd4f47
Harshitha-Renikindi <hrenikinidi@msitprogram.net>
2024-03-05 14:40:47 +0530

    testcase for power

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 class Calculator {
-    public int power(int a, int b) {
+    public int powerof(int a, int b) {
         int ans = 1;
         for (int i = 0; i < b; i++) {
             ans *= a;
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     Calculator calculator = new Calculator();
     // Your code for testing the remaining methods goes here
-    
+    System.out.print(calculator.powerof(2,3));
 }
 
