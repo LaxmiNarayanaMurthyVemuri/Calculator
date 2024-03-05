c1c40f2bb1283deb9d68ee5a9c6e042fb09cf11b
ellurunithin <162140533+ellurunithin@users.noreply.github.com>
2024-03-05 16:07:05 +0530

    Revert "Multi updated"

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,22 @@
 class Calculator {
-    public double multiply(int a, int b) {
-        int result = a*b;
+MultiUpdated
+    public double multiply(double... numbers) {
+        if (numbers.length == 0) {
+            return 0;
+        }
+        double result = 1.0;
+        for (double number : numbers) {
+            result *= number;
+        }
         return result;
+    public int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    public int findFloor(double num) {
+        return (int) num; 
     }
         public int findFloor(double num) {
         return (int) num; 
     }
 }
+
