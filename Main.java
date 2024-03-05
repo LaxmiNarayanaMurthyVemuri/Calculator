f732ec78463870dbce65ad9b2c1d3e151474f42d
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2024-03-05 17:08:59 +0530

    fixed all the test cases

--- Main.java
+++ Main.java
@@ -1,36 +1,42 @@
 
 
 public class Main {
-    // Your code for testing the remaining methods goes here
+    public static void main(String[] args) {
+        
+        // Your code for testing the remaining methods goes here   
+        Calculator calculator = new Calculator();
 
-    System.out.print(calculator.powerof(2,3));
+        // Test for powerof method
+        calculator.powerof(2, 3);
+        calculator.powerof(0, 0);
 
+        // Test for ceil method
+        calculator.ceil(12.5);
+        calculator.ceil(10);
 
+        // Test for division method
+        calculator.division(6, 2);
+        calculator.division(0, 0);
 
-    int result = calculator.division(6, 3);
-    System.out.println(result);
-}
+        // Test for sub method
+        calculator.sub(3, 4);
+        calculator.sub(0, 0);
+        
+        // Test for SquareRoot method
+        calculator.SquareRoot(25);
+        calculator.SquareRoot(17);
 
+        // Test for add method
+        calculator.add(3, 4);
+        calculator.add(2, 0);
 
-    System.out.println(calculator.sub(1,2));
-
-
-
-    
-
-public static void main(String[] args) {
-   
-    Calculator calculator = new Calculator();
-    int a= calculator.add(2,3);
-    System.out.println(a);
-    
-   int ans=calculator.findFloor(5.8);
-    System.out.println(ans);
-
-   double result=calculator.multiply(2,3);
-    System.out.println(result);
-
-}
+        // Test for multiply method
+        double[] a = {1, 2, 3, 4};
+        calculator.multiply(a);
+        calculator.multiply(a);
+        
+        
+    }
 
 }
    
