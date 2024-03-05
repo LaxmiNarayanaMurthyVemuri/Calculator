f64d78a619fbbd1d584fa364d8a914a66aee8170
Harshitha-Renikindi <hrenikinidi@msitprogram.net>
2024-03-05 14:29:54 +0530

    loop for iteration

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ public class Main {
     Calculator calculator = new Calculator();
     // Your code for testing the remaining methods goes here
     public int power(int a,int b){
-        
+        int ans=a;
+        for(int i=0;i<b;i++){
+            ans=ans*a;
+        }
     }
 }
 
