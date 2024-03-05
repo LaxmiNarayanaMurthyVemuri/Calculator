df2e3da45b26f3493fc2b2594250360f051d1b03
ellurunithin <nithinelluru12345@msitprogram.net>
2024-03-05 15:04:00 +0530

    burr

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,10 @@ class Calculator {
     public int add(int a, int b) {
         int sum = a + b;
         return sum;
+    }
+
     public int findFloor(double num) {
-        return (int) num; 
+        return (int) num;
 
     }
 }
-
